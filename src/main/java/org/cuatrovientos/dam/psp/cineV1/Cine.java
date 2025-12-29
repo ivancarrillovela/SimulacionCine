06a66ab5dac02920a7bfcbbf1106e963df6e0513
Ivan <ivancarrillovela@gmail.com>
2025-12-29 16:30:31 +0100

    Creados atributos para el recurso compartido 'Cine' y metodo synchronized para poder vender entradas

--- src/main/java/org/cuatrovientos/dam/psp/cineV1/Cine.java
+++ src/main/java/org/cuatrovientos/dam/psp/cineV1/Cine.java
@@ -1,5 +1,24 @@
 package org.cuatrovientos.dam.psp.cineV1;
 
 public class Cine {
+	
+	private String nombre;
+	private int asientosDisponibles;
+	private int entradasVendidas;
+	
+	public Cine(String nombre, int asientos) {
+		super();
+		this.nombre = nombre;
+		this.asientosDisponibles = asientos;
+	}
+	
+	public boolean venderEntrada() {
+		if (asientosDisponibles < 0) {
+			asientosDisponibles--;
+			entradasVendidas++;
+			return true;
+		}
+		return false;
+	}
 
 }
