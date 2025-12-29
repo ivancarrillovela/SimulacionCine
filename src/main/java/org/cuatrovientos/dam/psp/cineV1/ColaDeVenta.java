b0cbb6ffbf3161e1606e6a893b4cfb52bcc048aa
Ivan <ivancarrillovela@gmail.com>
2025-12-29 17:35:19 +0100

    Creados atributos y metodos synchronized que gestionan la ColaDeVenta

--- src/main/java/org/cuatrovientos/dam/psp/cineV1/ColaDeVenta.java
+++ src/main/java/org/cuatrovientos/dam/psp/cineV1/ColaDeVenta.java
@@ -1,5 +1,23 @@
 package org.cuatrovientos.dam.psp.cineV1;
 
-public class ColaDeVenta {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColaDeVenta{
+	
+	private Queue<Cliente> colaInfinita;
+
+	public ColaDeVenta() {
+		super();
+		this.colaInfinita = new LinkedList<>();
+	}
+	
+	public synchronized void anadirCliente(Cliente cliente) {
+		this.colaInfinita.add(cliente);
+	}
+	
+	public synchronized boolean estaVacia() {
+		return colaInfinita.isEmpty();
+	}
 
 }
