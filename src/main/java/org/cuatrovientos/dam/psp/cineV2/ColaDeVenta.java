c8f7303021e24320b946ab3f6ed211fbc10b14d8
Ivan <ivancarrillovela@gmail.com>
2026-01-02 19:22:39 +0100

    Añadido semaforo a la clase ColaDeVenta para poder controlar el aforo maximo

--- src/main/java/org/cuatrovientos/dam/psp/cineV2/ColaDeVenta.java
+++ src/main/java/org/cuatrovientos/dam/psp/cineV2/ColaDeVenta.java
@@ -2,14 +2,17 @@ package org.cuatrovientos.dam.psp.cineV2;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.Semaphore;
 
 public class ColaDeVenta{
 	
 	private int id;
 	private Queue<Cliente> cola = new LinkedList<>();
+	private Semaphore semaforoAforo;
 
-	public ColaDeVenta(int id) {
+	public ColaDeVenta(int id, int aforoMaximo) {
 		this.id = id;
+		this.semaforoAforo = new Semaphore(aforoMaximo);
 	}
 	
 	public synchronized void anadirCliente(Cliente cliente) {
